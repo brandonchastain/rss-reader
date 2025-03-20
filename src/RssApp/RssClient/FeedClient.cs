fccf173dd6d8a063597536b689ba0df495c0f2f2
Brandon Chastain <brandonchastain@Brandons-MacBook-Air.local>
2025-03-19 19:34:42 -0700

    upgrade to .net 9

--- src/RssApp/RssClient/FeedClient.cs
+++ src/RssApp/RssClient/FeedClient.cs
@@ -28,7 +28,7 @@ public class FeedClient : IFeedClient
         var items = new List<NewsFeedItem>();
         foreach (var url in urls)
         {
-            items.AddRange(await this.GetFeedItems(url, 0));
+            items.AddRange(await this.GetFeedItems(url));
         }
         
         var sorted = items.OrderByDescending(i => i.ParsedDate);
@@ -37,26 +37,33 @@ public class FeedClient : IFeedClient
 
     public async Task<IEnumerable<NewsFeedItem>> GetFeedItems(string url, int page)
     {
+        var items = await this.GetFeedItems(url);
+        return items
+            .Skip(page * PageSize)
+            .Take(PageSize);
+    }
+
+    private async Task<IEnumerable<NewsFeedItem>> GetFeedItems(string url)
+    {
         if (!feedCache.TryGetValue(url, out string response))
         {
             var httpRes = await this.httpClient.GetAsync(url);
             response = await httpRes.Content.ReadAsStringAsync();
-            feedCache.Set(url, response);
+            feedCache.Set(url, response, TimeSpan.FromMinutes(2));
         }
 
         response = feedCache.Get<string>(url);
 
         // this.logger.LogInformation(response);
         var hidden = this.hiddenItems.GetHidden();
-        var items = this.deserializer.FromString(response).ToList();;
+        var items = this.deserializer.FromString(response).ToList();
         foreach (var item in items)
         {
             item.FeedUrl = url;
         }
         var result = items.DistinctBy(i => i.Id)
-            .Where(i => !hidden.Contains(i.Id))
-            .Skip(page * PageSize)
-            .Take(PageSize);
+            .OrderByDescending(i => i.ParsedDate)
+            .Where(i => !hidden.Contains(i.Id));
         return result;
     }
 
