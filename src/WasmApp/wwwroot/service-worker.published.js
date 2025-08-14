33d5593c970668ad8a3882dda0307f4575fed651
Brandon Chastain <brandonchastain@gmail.com>
2025-08-13 21:09:13 -0700

    fixed auth

--- src/WasmApp/wwwroot/service-worker.published.js
+++ src/WasmApp/wwwroot/service-worker.published.js
@@ -14,7 +14,9 @@ const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 // Replace with your base path if you are hosting on a subfolder. Ensure there is a trailing '/'.
 const base = "/";
 const baseUrl = new URL(base, self.origin);
-const manifestUrlList = self.assetsManifest.assets.map(asset => new URL(asset.url, baseUrl).href);
+const manifestUrlList = 
+self.assetsManifest.assets.map(asset => new URL(asset.url, baseUrl).href);
+const authUrlPattern = "/.auth/"
 
 async function onInstall(event) {
     console.info('Service worker: Install');
@@ -44,7 +46,8 @@ async function onFetch(event) {
         // unless that request is for an offline resource.
         // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
         const shouldServeIndexHtml = event.request.mode === 'navigate'
-            && !manifestUrlList.some(url => url === event.request.url);
+            && !manifestUrlList.some(url => url === event.request.url)
+            && !event.request.url.includes(authUrlPattern);
 
         const request = shouldServeIndexHtml ? 'index.html' : event.request;
         const cache = await caches.open(cacheName);
