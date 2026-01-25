9f105bd7df991ab8e1da9291fc31f4473c39bc1d
Brandon Chastain <brandonchastain@gmail.com>
2026-01-25 02:54:03 -0800

    add loading message to frontend for cold start handling

--- src/Server/scripts/cleanupssh.sh
+++
@@ -1,9 +0,0 @@
-az network nsg rule delete \
-    --resource-group rss-reader-linux-group \
-    --nsg-name rss-linux-vmNSG \
-    --name default-allow-ssh
-
-az network nsg rule delete \
-    --resource-group rss-reader-linux-group \
-    --nsg-name rss-linux-vmNSG \
-    --name open-port-22
